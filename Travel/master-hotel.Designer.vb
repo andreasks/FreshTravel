fb01979448787620064701390d79d3db2faae0dd
Andreas Kristian Santos <andreascristian27@gmail.com>
2019-11-11 04:43:41 +0700

    Master Hotel

--- Travel/master-hotel.Designer.vb
+++ Travel/master-hotel.Designer.vb
@@ -27,7 +27,6 @@ Partial Class master_hotel
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
@@ -44,8 +43,22 @@ Partial Class master_hotel
         Me.TextBox6 = New System.Windows.Forms.TextBox()
         Me.TextBox7 = New System.Windows.Forms.TextBox()
         Me.Label10 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Edit = New System.Windows.Forms.GroupBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Edit.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -53,16 +66,18 @@ Partial Class master_hotel
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(13, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Lokasi"
+        Me.Label1.Text = "Provinsi"
         '
         'ComboBox1
         '
         Me.ComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
         Me.ComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(57, 10)
+        Me.ComboBox1.Items.AddRange(New Object() {"Jawa Timur", "Jawa Barat", "Jawa Tengah", "Daerah Istimewa Yogyakarta", "Banten", "Jakarta", "Bali", "NTT", "NTB"})
+        Me.ComboBox1.Location = New System.Drawing.Point(82, 10)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 1
@@ -70,32 +85,23 @@ Partial Class master_hotel
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(193, 13)
+        Me.Label2.Location = New System.Drawing.Point(13, 41)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Lokasi Baru"
+        Me.Label2.Text = "Kota"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(262, 10)
+        Me.TextBox1.Location = New System.Drawing.Point(82, 36)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 3
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(379, 8)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Save"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(13, 60)
+        Me.Label3.Location = New System.Drawing.Point(12, 65)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(63, 13)
         Me.Label3.TabIndex = 5
@@ -104,7 +110,7 @@ Partial Class master_hotel
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(268, 60)
+        Me.Label4.Location = New System.Drawing.Point(268, 65)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(47, 13)
         Me.Label4.TabIndex = 6
@@ -113,7 +119,7 @@ Partial Class master_hotel
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(434, 60)
+        Me.Label5.Location = New System.Drawing.Point(434, 65)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(46, 13)
         Me.Label5.TabIndex = 7
@@ -122,7 +128,7 @@ Partial Class master_hotel
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(602, 60)
+        Me.Label6.Location = New System.Drawing.Point(602, 65)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(40, 13)
         Me.Label6.TabIndex = 8
@@ -130,35 +136,38 @@ Partial Class master_hotel
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(82, 57)
+        Me.TextBox2.Location = New System.Drawing.Point(82, 62)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(171, 20)
         Me.TextBox2.TabIndex = 9
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(321, 57)
+        Me.TextBox3.Location = New System.Drawing.Point(321, 62)
+        Me.TextBox3.MaxLength = 8
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(98, 20)
         Me.TextBox3.TabIndex = 10
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(486, 57)
+        Me.TextBox4.Location = New System.Drawing.Point(486, 62)
+        Me.TextBox4.MaxLength = 8
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(98, 20)
         Me.TextBox4.TabIndex = 11
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(648, 57)
+        Me.TextBox5.Location = New System.Drawing.Point(648, 62)
+        Me.TextBox5.MaxLength = 8
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.Size = New System.Drawing.Size(98, 20)
         Me.TextBox5.TabIndex = 12
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(766, 55)
+        Me.Button2.Location = New System.Drawing.Point(766, 60)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 13
@@ -170,7 +179,7 @@ Partial Class master_hotel
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(12, 167)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(829, 184)
+        Me.DataGridView1.Size = New System.Drawing.Size(829, 197)
         Me.DataGridView1.TabIndex = 14
         '
         'Label7
@@ -195,7 +204,7 @@ Partial Class master_hotel
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(405, 111)
+        Me.Label9.Location = New System.Drawing.Point(394, 107)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(75, 25)
         Me.Label9.TabIndex = 17
@@ -210,7 +219,7 @@ Partial Class master_hotel
         '
         'TextBox7
         '
-        Me.TextBox7.Location = New System.Drawing.Point(514, 137)
+        Me.TextBox7.Location = New System.Drawing.Point(505, 137)
         Me.TextBox7.Name = "TextBox7"
         Me.TextBox7.Size = New System.Drawing.Size(327, 20)
         Me.TextBox7.TabIndex = 19
@@ -220,25 +229,149 @@ Partial Class master_hotel
         Me.Label10.AutoSize = True
         Me.Label10.Location = New System.Drawing.Point(470, 140)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(38, 13)
+        Me.Label10.Size = New System.Drawing.Size(29, 13)
         Me.Label10.TabIndex = 20
-        Me.Label10.Text = "Lokasi"
+        Me.Label10.Text = "Kota"
         '
-        'Button3
+        'Edit
         '
-        Me.Button3.Location = New System.Drawing.Point(766, 357)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 21
-        Me.Button3.Text = "Edit"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Edit.Controls.Add(Me.Button1)
+        Me.Edit.Controls.Add(Me.TextBox12)
+        Me.Edit.Controls.Add(Me.ComboBox2)
+        Me.Edit.Controls.Add(Me.TextBox11)
+        Me.Edit.Controls.Add(Me.TextBox10)
+        Me.Edit.Controls.Add(Me.TextBox9)
+        Me.Edit.Controls.Add(Me.TextBox8)
+        Me.Edit.Controls.Add(Me.Label16)
+        Me.Edit.Controls.Add(Me.Label15)
+        Me.Edit.Controls.Add(Me.Label14)
+        Me.Edit.Controls.Add(Me.Label13)
+        Me.Edit.Controls.Add(Me.Label12)
+        Me.Edit.Controls.Add(Me.Label11)
+        Me.Edit.Location = New System.Drawing.Point(188, 10)
+        Me.Edit.Name = "Edit"
+        Me.Edit.Size = New System.Drawing.Size(416, 318)
+        Me.Edit.TabIndex = 21
+        Me.Edit.TabStop = False
+        Me.Edit.Text = "Edit Data Hotel"
+        Me.Edit.Visible = False
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(82, 59)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(63, 13)
+        Me.Label11.TabIndex = 0
+        Me.Label11.Text = "Nama Hotel"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(82, 89)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(44, 13)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "Provinsi"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(82, 119)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(29, 13)
+        Me.Label13.TabIndex = 2
+        Me.Label13.Text = "Kota"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(82, 149)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(47, 13)
+        Me.Label14.TabIndex = 3
+        Me.Label14.Text = "Standart"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(82, 179)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(46, 13)
+        Me.Label15.TabIndex = 4
+        Me.Label15.Text = "Superior"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(82, 209)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(40, 13)
+        Me.Label16.TabIndex = 5
+        Me.Label16.Text = "Deluxe"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(154, 56)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox8.TabIndex = 6
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(154, 116)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox9.TabIndex = 7
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(154, 146)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox10.TabIndex = 8
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(154, 176)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox11.TabIndex = 9
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.ComboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Jawa Timur", "Jawa Barat", "Jawa Tengah", "Daerah Istimewa Yogyakarta", "Banten", "Jakarta", "Bali", "NTT", "NTB"})
+        Me.ComboBox2.Location = New System.Drawing.Point(154, 86)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 22
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(154, 206)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox12.TabIndex = 23
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(285, 264)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 24
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'master_hotel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(853, 387)
-        Me.Controls.Add(Me.Button3)
+        Me.ClientSize = New System.Drawing.Size(853, 376)
+        Me.Controls.Add(Me.Edit)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.TextBox7)
         Me.Controls.Add(Me.TextBox6)
@@ -255,7 +388,6 @@ Partial Class master_hotel
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ComboBox1)
@@ -266,6 +398,8 @@ Partial Class master_hotel
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "master_hotel"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Edit.ResumeLayout(False)
+        Me.Edit.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -275,7 +409,6 @@ Partial Class master_hotel
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
@@ -292,5 +425,18 @@ Partial Class master_hotel
     Friend WithEvents TextBox6 As TextBox
     Friend WithEvents TextBox7 As TextBox
     Friend WithEvents Label10 As Label
-    Friend WithEvents Button3 As Button
+    Friend WithEvents Edit As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox12 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox11 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
 End Class
--- Travel/master-hotel.vb
+++ Travel/master-hotel.vb
@@ -4,62 +4,33 @@ Public Class master_hotel
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Try
-            connectDatabase()
-            xCommand = New MySqlCommand("insert into daerah (lokasi) values ('" + LCase(TextBox1.Text) + "')", xConnection)
-            xCommand.ExecuteNonQuery()
-            TextBox1.Text = ""
-
-        Catch ex As Exception
-            MsgBox("Maaf Data Telah Tersedia")
-            TextBox1.Text = ""
-        End Try
-
-
-    End Sub
 
     Private Sub master_hotel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.MdiParent = main_menu
         connectDatabase()
-        xCommand = New MySqlCommand("select lokasi from daerah  ", xConnection)
-        xReader = xCommand.ExecuteReader
-        While xReader.Read
-            ComboBox1.Items.Add(xReader.GetValue(0))
-        End While
-        xReader.Close()
         isi_dgv()
     End Sub
 
 
     Private Sub isi_dgv()
         xDataSet.Clear()
-        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, lokasi FROM master_hotel ", xConnection)
+        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, provinsi, kota FROM master_hotel ", xConnection)
         xAdapter.Fill(xDataSet, "master_hotel")
         xView = xDataSet.Tables("master_hotel").DefaultView
         DataGridView1.DataSource = xView
-        DataGridView1.Columns.Item(0).HeaderText = "Nama Hotel"
-        DataGridView1.Columns.Item(1).HeaderText = "Standart"
-        DataGridView1.Columns.Item(2).HeaderText = "Superior"
-        DataGridView1.Columns.Item(3).HeaderText = "Deluxe"
-        DataGridView1.Columns.Item(4).HeaderText = "Lokasi"
-
-        DataGridView1.Columns.Item(0).Width = 200
-        DataGridView1.Columns.Item(1).Width = 150
-        DataGridView1.Columns.Item(2).Width = 150
-        DataGridView1.Columns.Item(3).Width = 150
-        DataGridView1.Columns.Item(4).Width = 100
-
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Try
-            xCommand = New MySqlCommand("insert into master_hotel (lokasi, nama_hotel, standart, superior, deluxe) values ('" + ComboBox1.Text + "', '" + TextBox2.Text + "', " + TextBox3.Text + ", " + TextBox4.Text + ", " + TextBox5.Text + ")", xConnection)
+            'xConnection.Open()
+            xCommand = New MySqlCommand("insert into master_hotel (provinsi, kota, nama_hotel, standart, superior, deluxe) values ('" + ComboBox1.SelectedItem.ToString + "','" + TextBox1.Text + "', '" + TextBox2.Text + "', '" + TextBox3.Text + "', '" + TextBox4.Text + "', '" + TextBox5.Text + "')", xConnection)
             xCommand.ExecuteNonQuery()
+            TextBox1.Text = ""
             TextBox2.Text = ""
             TextBox3.Text = ""
             TextBox4.Text = ""
             TextBox5.Text = ""
+            MsgBox("Data Berhasil Diinput")
             isi_dgv()
         Catch ex As Exception
             MsgBox("Ada Data yang Salah / Belum di Isi")
@@ -69,39 +40,68 @@ Public Class master_hotel
 
     Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
         xDataSet.Clear()
-        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, lokasi FROM master_hotel WHERE nama_hotel LIKE '%" & TextBox6.Text & "%' ", xConnection)
+        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, provinsi, kota FROM master_hotel WHERE nama_hotel LIKE '%" & TextBox6.Text & "%' ", xConnection)
         xAdapter.Fill(xDataSet, "master_hotel")
         xView = xDataSet.Tables("master_hotel").DefaultView
         DataGridView1.DataSource = xView
-        DataGridView1.Columns.Item(0).HeaderText = "Nama Hotel"
-        DataGridView1.Columns.Item(1).HeaderText = "Standart"
-        DataGridView1.Columns.Item(2).HeaderText = "Superior"
-        DataGridView1.Columns.Item(3).HeaderText = "Deluxe"
-        DataGridView1.Columns.Item(4).HeaderText = "Lokasi"
-
-        DataGridView1.Columns.Item(0).Width = 200
-        DataGridView1.Columns.Item(1).Width = 150
-        DataGridView1.Columns.Item(2).Width = 150
-        DataGridView1.Columns.Item(3).Width = 150
-        DataGridView1.Columns.Item(4).Width = 100
+       
     End Sub
 
     Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs) Handles TextBox7.TextChanged
         xDataSet.Clear()
-        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, lokasi FROM master_hotel WHERE lokasi LIKE '%" & TextBox7.Text & "%' ", xConnection)
+        xAdapter = New MySqlDataAdapter("SELECT nama_hotel, standart, superior, deluxe, provinsi, kota FROM master_hotel WHERE kota LIKE '%" & TextBox7.Text & "%' ", xConnection)
         xAdapter.Fill(xDataSet, "master_hotel")
         xView = xDataSet.Tables("master_hotel").DefaultView
         DataGridView1.DataSource = xView
-        DataGridView1.Columns.Item(0).HeaderText = "Nama Hotel"
-        DataGridView1.Columns.Item(1).HeaderText = "Standart"
-        DataGridView1.Columns.Item(2).HeaderText = "Superior"
-        DataGridView1.Columns.Item(3).HeaderText = "Deluxe"
-        DataGridView1.Columns.Item(4).HeaderText = "Lokasi"
+    End Sub
 
-        DataGridView1.Columns.Item(0).Width = 200
-        DataGridView1.Columns.Item(1).Width = 150
-        DataGridView1.Columns.Item(2).Width = 150
-        DataGridView1.Columns.Item(3).Width = 150
-        DataGridView1.Columns.Item(4).Width = 100
+    Private Sub TextBox3_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox3.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox4.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox5_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox5.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If e.ColumnIndex = 0 Or e.ColumnIndex = 1 Or e.ColumnIndex = 2 Or e.ColumnIndex = 3 Or e.ColumnIndex = 4 Or e.ColumnIndex = 5 Or e.ColumnIndex = 6 Or e.ColumnIndex = 7 Or e.ColumnIndex = 8 Or e.ColumnIndex = 9 Then
+            Edit.Visible = True
+            If DataGridView1.CurrentRow.Index <> DataGridView1.NewRowIndex Then
+
+                
+            End If
+            xReader.Close()
+        End If
     End Sub
 End Class
